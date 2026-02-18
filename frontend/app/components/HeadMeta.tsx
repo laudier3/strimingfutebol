885131394b68e6b5b415d378e985fc8ed6114b62
root <root@hk.hk>
2026-02-17 23:02:29 -0300

    primeiro commit

--- frontend/app/components/HeadMeta.tsx
+++ frontend/app/components/HeadMeta.tsx
@@ -19,6 +19,7 @@ export const HeadMeta = ({ title, description, imageUrl, url }: HeadMetaProps) =
     <meta property="og:title" content={title} />
     <meta property="og:description" content={description} />
     <meta property="og:url" content={url} />
+    <meta property="og:site_name" content="Futebol ao Vivo" />
     <meta property="og:image" content={imageUrl} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
@@ -28,5 +29,9 @@ export const HeadMeta = ({ title, description, imageUrl, url }: HeadMetaProps) =
     <meta name="twitter:title" content={title} />
     <meta name="twitter:description" content={description} />
     <meta name="twitter:image" content={imageUrl} />
+
+    {/* Favicon */}
+    <link rel="icon" href="/favicon.ico" />
+    <link rel="apple-touch-icon" href="/apple-icon.png" />
   </Head>
 )
